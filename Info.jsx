0dcf328f57a3c04c3eeecfdac727e4971e0b12f6
Aishwarya <72559196+aishwarya5048@users.noreply.github.com>
2021-06-26 19:11:26 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info(){
+  return(
+    <div className="note">
+      <h1>Feedback</h1>
+      <p> I started my 1 st bootcamp in ShapeAI of Web Dev with Javascript and React.js. It was quite Awesome experience I started enjoying to learn with this bootcamp</p>
+    </div>
+    );
+  }
+export default Info;
